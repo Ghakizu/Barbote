c894adc554f73394a22b00553b9f7d4e8d6e75d8
Ghakizu <cmalanda1999@outlook.fr>
2019-01-17 06:00:06 +0000

    json files

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 
 import logging
 import time
+import json
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -12,10 +13,24 @@ logger.addHandler(handler)
 
 TOKEN = 'NTM0ODU2NDAxMzc4NDc2MDQy.Dx_vkw.bIVvr5GumIe0XmOITL1yYzHS7NA'
 
-act = discord.Activity(type=discord.ActivityType.watching, name="my master developping me UwU")
+act = discord.Activity(type=discord.ActivityType.playing, name=" watching my master developping me UwU")
 Barbote = commands.Bot(command_prefix="?!", description="Hello I'm Barbote !\n I'm useless at this point",
         activity=act)
-dic = {}
+
+try:
+    jsondic = open('dico.json', 'r')
+    dic = json.load(jsondic)
+except FileNotFoundError:
+    print("There is no saved dictionnary")
+    dic = {}
+
+try:
+    jsoncat = open('cat.json', 'r')
+    cat = json.load(jsoncat)
+except FileNotFoundError:
+    print("There is no saved categories")
+    cat = []
+
 Ghakid = 175392863587139584
 Ghakizu = Barbote.get_user(Ghakid)
 
@@ -26,7 +41,10 @@ async def on_ready():
     print(Barbote.user.id)
     print('--------------')
 
-"""
+@Barbote.event
+async def on_member_update(before, after):
+    check(after)
+
 @Barbote.event
 async def on_message(message):
     if message.author.bot:
@@ -34,7 +52,7 @@ async def on_message(message):
     if type(message.channel) == discord.DMChannel or type(message.content) == discord.GroupChannel:
         m = "I received a message from : {0}#{1}\nIt said : {2}".format(message.author.name, message.author.discriminator, message.content)
         await Barbote.Ghakizu.dm_channel.send(m)
-"""
+    await Barbote.process_commands(message)
 
 @Barbote.command()
 async def ping(ctx):
@@ -44,38 +62,42 @@ async def ping(ctx):
 
 @Barbote.command()
 async def setrole(ctx, rolesetted:discord.Role, *roles:discord.Role):
-    print(len(roles))
-    for x in roles:
-        print("{} : {}".format(x, x.id))
-    print(rolesetted)
-    guild = ctx.guild
-    if guild is not None:
-        print(guild.name)
+    if rolesetted is not None and len(roles) > 0:
+        print("Number of roles to add : {}".format(len(roles)))
+        for x in roles:
+            print("{} : {}".format(x, x.id))
+        print(rolesetted)
+        guild = ctx.guild
+        if guild is not None:
+            print(guild.name)
+        else:
+            print("Guild is None")
+        try:
+            async with ctx.channel.typing():
+                c = addroles(rolesetted, roles)
+            await ctx.send('Done!\n{0} roles added to the category {1}'.format(c,
+                rolesetted.name))
+            for key in dic.keys():
+                print("{} : {}".format(guild.get_role(int(key)).name,
+                    guild.get_role(int(dic.get(key))).name))
+            save(dic)
+        except Exception as e:
+            await ctx.send("Debug error : {0}".format(e))
+            print(e)
+            logger.fatal(e)
     else:
-        print("Guild is None")
-    try:
-        async with ctx.channel.typing():
-            c = addroles(rolesetted, roles)
-        await ctx.send('Done!\n{0} roles added to the category {1}'.format(c,
-            rolesetted.name))
-        for key in dic.keys():
-            print("{} : {}".format(guild.get_role(int(key)).name,
-                guild.get_role(int(dic.get(key))).name))
-    except Exception as e:
-        await ctx.send("Wlh c'est cassé\nVoici l'erreur: {0}".format(e))
-        print(e)
-        logger.fatal(e)
+        ctx.send("Error : Not enought argument")
 
 @Barbote.command()
 async def checkuser(ctx, user:discord.Member):
     ctx.send("Checking {0}...".format(user))
     async with ctx.channel.typing():
-        // TODO
+        # TODO
         ctx.send("Not yet implemented")
 
 @Barbote.command()
 async def checkall(ctx):
-    // TODO
+    # TODO
     ctx.send("Not yet implemented")
 
 def addroles(rolesetted, roles):
@@ -87,8 +109,17 @@ def addroles(rolesetted, roles):
     return c
 
 def check(user):
-    // TODO
+    # TODO
     pass
+
+def save(dico):
+    for key in dic.keys():
+        if not dic.get(key) in cat:
+            cat.append(dic.get(key))
+    jsondic = open('dico.json', 'w')
+    jsoncat = open('cat.json', 'w')
+    json.dump(dico, jsondic)
+    json.dump(cat, jsoncat)
 
 try:
     Barbote.run(TOKEN)
